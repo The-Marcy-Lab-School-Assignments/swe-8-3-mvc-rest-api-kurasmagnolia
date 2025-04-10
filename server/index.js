47fbe164cbd28453fb3e0b26f8f9b8a9deef826c
kurasmagnolia <kingmmcleod@outlook.com>
2025-04-10 12:53:41 -0400

    added contollers + endpoints secitdon commenting & added listener for server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@
 const express = require('express');
 const path = require('path');
 const getId = require('./utils/getId');
+const PORT = 8080;
 
 ////////////////////////
 // Constants
@@ -65,3 +66,16 @@ const serveStatic = express.static(pathToFrontendDist);
 app.use(logRoutes); // Print out every incoming request
 app.use(serveStatic); // Serve static public/ content
 app.use(express.json());
+
+////////////////////////
+// Controllers
+////////////////////////
+
+////////////////////////
+// Endpoints
+////////////////////////
+
+////////////////////////
+// Listener
+////////////////////////
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
